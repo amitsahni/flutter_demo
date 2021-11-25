e5d07ad05762b70789a428a23d200cc54b6f0fe5
Amit Singh <asingh@tila.com>
2021-11-25 11:21:18 +0530

    Changes Use case added

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:f_d/app/main.dart';
+import 'package:f_d/src/app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(RecipeApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
